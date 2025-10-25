dd4d209ee4592f8d64e94a1b702209c3d9a3812e
JafarRababah <j3fr.rababah@gmail.com>
2025-10-25 18:11:54 +0300

    Add logout

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,19 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EmployeesManagment.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EmployeesManagment.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
         public IActionResult AccessDenied()
         {
             return View();
         }
-       
-            public IActionResult Login()
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Login", "Account");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string email, string password)
+        {
+            var result = await _signInManager.PasswordSignInAsync(email, password, false, lockoutOnFailure: false);
+
+            if (result.Succeeded)
             {
-                return View();
+                return RedirectToAction("Index", "Home");
             }
-        
+
+            ViewBag.ErrorMessage = "Invalid login attempt";
+            return View();
+        }
 
     }
 }
